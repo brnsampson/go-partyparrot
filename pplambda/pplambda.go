97005ccd9bad7d0beef8f21809d4a3ac0d644271
Brian Sampson <brnsampson@gmail.com>
2018-02-26 21:13:49 -0800

    Import referenced testing dir instead of github

--- pplambda/pplambda.go
+++ pplambda/pplambda.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
-	"experiment/pkg/partyparrot"
 	"fmt"
 	"net/url"
 	"os"
@@ -12,6 +11,7 @@ import (
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/brnsampson/go-partyparrot/partyparrot"
 )
 
 var (
