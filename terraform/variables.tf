206fe38dd0c92edf8aae85255236db5759e08b7d
Brian Sampson <brnsampson@gmail.com>
2018-04-03 20:34:25 -0700

    Initial add of terraform files

---
+++ terraform/variables.tf
@@ -0,0 +1,16 @@
+variable "region" {
+  type = "string"
+  default = "us-west-2"
+}
+
+variable "cert_arn" {
+  type = "string"
+}
+
+variable "deploy_file" {
+  type = "string"
+}
+
+variable "env" {
+  type = "string"
+}
